8557f413335044a98e2fe6e848e86e17c6f5cf23
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-05-21 16:28:53 +1200

    Add ExecutorService and Thread prediction files

---
+++ src/main/java/group16/executor/executorService/threadPrediction/FuturePredictor.java
@@ -0,0 +1,12 @@
+package group16.executor.executorService.threadPrediction;
+
+public interface FuturePredictor {
+
+    /***
+     * Given a reading now, will predict a value in the future given some implementation and previous calls to this
+     * method.
+     * @param number A number to predict from
+     * @return A predicted numbe given this and previous calls to this method
+     */
+    int predictNumberInFuture(int number);
+}
