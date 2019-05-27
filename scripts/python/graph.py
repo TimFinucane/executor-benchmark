78aff88c217b9d58da2587b2c293bc36c499378b
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-05-28 10:16:59 +1200

    Add graphings for other data types

--- scripts/python/graph.py
+++ scripts/python/graph.py
@@ -3,25 +3,34 @@ import json
 import matplotlib.pyplot as plt
 
 
+def graph(x, y, xlabel, ylabel):
+    plt.plot(x, y)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.show()
+
+
 def create_metrics_graphs(metrics):
     local_metrics = metrics['local']
     global_metrics = metrics['global']
 
-    first_sample = global_metrics['globalDataSamples'][0]
+    # Local Plotting
+    first_local_sample_time = local_metrics['taskDataSamples'][0]['taskSubmitTime']
+    task_start_times = [l['taskSubmitTime'] - first_local_sample_time for l in local_metrics['taskDataSamples']]
+    task_completion_times = [l['taskCompletionTime'] for l in local_metrics['taskDataSamples']]
 
-    sample_times = [g['sampleTime'] - first_sample['sampleTime'] for g in global_metrics['globalDataSamples']]
+    graph(task_start_times, task_completion_times, 'Time (nano seconds)', 'Task Completion Time (seconds)')
+
+    # Global Plotting
+    first_global_sample = global_metrics['globalDataSamples'][0]
+    sample_times = [g['sampleTime'] - first_global_sample['sampleTime'] for g in global_metrics['globalDataSamples']]
     cpu_loads = [g['cpuLoad'] for g in global_metrics['globalDataSamples']]
     thread_counts = [g['threadCount'] for g in global_metrics['globalDataSamples']]
+    responsiveness = [g['responsiveWorkCompleted'] for g in global_metrics['globalDataSamples']]
 
-    plt.plot(sample_times, cpu_loads)
-    plt.ylabel('CPU Load (%)')
-    plt.xlabel('Time (nano seconds)')
-    plt.show()
-
-    plt.plot(sample_times, thread_counts)
-    plt.ylabel('Thread Count')
-    plt.xlabel('Time (nano seconds)')
-    plt.show()
+    graph(sample_times, cpu_loads, 'Time (nano seconds)', 'CPU Load (%)')
+    graph(sample_times, thread_counts, 'Time (nano seconds)', 'Thread Count')
+    graph(sample_times, responsiveness, 'Time (nano seconds)', 'Responsiveness')
 
 
 if __name__ == '__main__':
