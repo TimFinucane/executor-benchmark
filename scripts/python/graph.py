e8324aa47abaf3612c4ef2540da8a5e4e5fc2bc7
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-05-28 08:52:19 +1200

    Get some better graphs plotting

--- scripts/python/graph.py
+++ scripts/python/graph.py
@@ -7,10 +7,22 @@ def create_metrics_graphs(metrics):
     local_metrics = metrics['local']
     global_metrics = metrics['global']
 
-    plt.plot(local_metrics['taskCompletionTimes'])
-    plt.ylabel('some numbers')
+    first_sample = global_metrics['globalDataSamples'][0]
+
+    sample_times = [g['sampleTime'] - first_sample['sampleTime'] for g in global_metrics['globalDataSamples']]
+    cpu_loads = [g['cpuLoad'] for g in global_metrics['globalDataSamples']]
+    thread_counts = [g['threadCount'] for g in global_metrics['globalDataSamples']]
+
+    plt.plot(sample_times, cpu_loads)
+    plt.ylabel('CPU Load (%)')
+    plt.xlabel('Time (nano seconds)')
     plt.show()
-    
+
+    plt.plot(sample_times, thread_counts)
+    plt.ylabel('Thread Count')
+    plt.xlabel('Time (nano seconds)')
+    plt.show()
+
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
